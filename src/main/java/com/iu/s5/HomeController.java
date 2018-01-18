e4de24e705544b04c06b947f9c8f6758abcc6171
jjy <j2y1012@hanmail.net>
2018-01-18 16:50:29 +0900

    file

--- src/main/java/com/iu/s5/HomeController.java
+++ src/main/java/com/iu/s5/HomeController.java
@@ -2,6 +2,7 @@ package com.iu.s5;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 /**
  * Handles requests for the application home page.
@@ -19,6 +22,13 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@RequestMapping(value="/fileTest", method=RequestMethod.POST)
+	public void fileTest(MultipartHttpServletRequest request) throws Exception{
+		MultipartFile f = request.getFile("f1");
+		System.out.println(f.getName());
+		System.out.println(f.getOriginalFilename());
+		System.out.println(f.getSize());
+	}
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -8,7 +8,11 @@
 <h1>
 	Hello world!  
 </h1>
-
+<form action="./fileTest" method="post" enctype="multipart/form-data"> 
+	<input type="text" name="id">
+	<input type="file" name="f1">
+	<button>button</button>
+</form>
 <P>  The time on the server is ${serverTime}. </P>
 </body>
 </html>
